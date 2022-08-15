780e10f504268573c7c9d69695079da6eddde877
jinx71 <karim.sazed@gmail.com>
2022-08-16 04:10:22 +0600

    Bank JS Add

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const accountStatus = {
     withdraw: 0,
     balance: 1250,
 }
-
+const today = new Date();
+const date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
 const accounts = document.getElementsByClassName('account');
 const userMail = document.getElementById('userMail');
 const logOutButton = document.getElementById('sign-out-button')
@@ -25,6 +26,10 @@ const closeModal = document.getElementById('close');
 const modalOverlayDeposite = document.getElementById('overlayDeposite');
 const modalDeposite = document.getElementById('dialogDeposite');
 const closeModalDeposite = document.getElementById('closeDeposite');
+
+const balanceSheet = document.getElementById('balanceSheet');
+const balanceSheetBody = document.getElementById('balanceSheetBody');
+const balanceSheetFoot = document.getElementById('balanceSheetFoot');
 
 userMail.innerText = 'User Mail : ' + document.cookie;
 
@@ -56,13 +61,54 @@ depositeButton.addEventListener('click', function () {
         depositeAmount.innerText = '$' + accountStatus.deposite;
         accountStatus.balance = accountStatus.balance + parseInt(depositeValue.value);
         balanceAmount.innerText = '$' + accountStatus.balance;
+        balanceSheet.classList.remove('hidden');
+        balanceSheetBody.innerHTML += `
+        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+        <th scope="row"
+            class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">`
+            + date + `
+        </th>
+        <td class="py-4 px-6">` +
+            '' + `
+        </td>
+        <td class="py-4 px-6">
+            `+ parseInt(depositeValue.value) + `
+        </td>
+        <td class="py-4 px-6">
+            `+ accountStatus.balance + `
+        </td>
 
+    </tr>
+    `
+        balanceSheetFoot.innerHTML = `
+        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+        <th scope="row"
+            class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">`+ 'Total' + `
+        </th>
+        <td class="py-4 px-6">` +
+            accountStatus.withdraw + `
+        </td>
+        <td class="py-4 px-6">
+            `+ accountStatus.deposite + `
+        </td>
+        <td class="py-4 px-6">
+            `+ accountStatus.balance + `
+        </td>
+
+    </tr>
+    `
+        depositeValue.value = '';
     }
-
 })
 withdrawButton.addEventListener('click', function () {
     // console.log(withdrawValue.value);
-    if (parseInt(withdrawValue.value) > accountStatus.balance) {
+    if (isNaN(parseInt(withdrawValue.value))) {
+        modalOverlayDeposite.classList.remove('hidden');
+        modalOverlayDeposite.classList.add('block');
+        modalDeposite.classList.remove('hidden');
+        modalDeposite.classList.add('block');
+    }
+    else if (parseInt(withdrawValue.value) > accountStatus.balance) {
         modalOverlay.classList.remove('hidden');
         modalOverlay.classList.add('block');
         modal.classList.remove('hidden');
@@ -72,8 +118,45 @@ withdrawButton.addEventListener('click', function () {
         withdrawAmount.innerText = '$' + accountStatus.withdraw;
         accountStatus.balance = accountStatus.balance - parseInt(withdrawValue.value);
         balanceAmount.innerText = '$' + accountStatus.balance;
+        balanceSheet.classList.remove('hidden');
+        balanceSheetBody.innerHTML += `
+        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+        <th scope="row"
+            class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">`
+            + date + `
+        </th>
+        <td class="py-4 px-6">` +
+            parseInt(withdrawValue.value) + `
+        </td>
+        <td class="py-4 px-6">
+            `+ '' + `
+        </td>
+        <td class="py-4 px-6">
+            `+ accountStatus.balance + `
+        </td>
 
+    </tr>
+    `
+        balanceSheetFoot.innerHTML = `
+        <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+        <th scope="row"
+            class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">`+ 'Total' + `
+        </th>
+        <td class="py-4 px-6">` +
+            accountStatus.withdraw + `
+        </td>
+        <td class="py-4 px-6">
+            `+ accountStatus.deposite + `
+        </td>
+        <td class="py-4 px-6">
+            `+ accountStatus.balance + `
+        </td>
+
+    </tr>
+    `
+        withdrawValue.value = '';
     }
+
 })
 closeModal.addEventListener('click', function () {
     modalOverlay.classList.remove('block');
