cc76cc950a26e2a5a560e5fc6f33554f1d5f85ad
jinx71 <karim.sazed@gmail.com>
2022-08-16 01:57:27 +0600

    Bank JS Add

--- js/script.js
+++ js/script.js
@@ -26,6 +26,13 @@ const withdrawValue = document.getElementById('withdraw');
 const depositeButton = document.getElementById('depositeButton');
 const withdrawButton = document.getElementById('withdrawButton');
 
+const modalOverlay = document.getElementById('overlay');
+const modal = document.getElementById('dialog');
+const closeModal = document.getElementById('close');
+
+const modalOverlayDeposite = document.getElementById('overlayDeposite');
+const modalDeposite = document.getElementById('dialogDeposite');
+const closeModalDeposite = document.getElementById('closeDeposite');
 
 balanceAmount.innerText = '$' + accountStatus.balance;
 depositeAmount.innerText = '$' + accountStatus.deposite;
@@ -62,16 +69,45 @@ logOutButton.addEventListener('click', function () {
 
 depositeButton.addEventListener('click', function () {
     // console.log(depositeValue.value);
-    accountStatus.deposite = accountStatus.deposite + parseInt(depositeValue.value);
-    depositeAmount.innerText = '$' + accountStatus.deposite;
-    accountStatus.balance = accountStatus.balance + parseInt(depositeValue.value);
-    balanceAmount.innerText = '$' + accountStatus.balance;
+    if (isNaN(parseInt(depositeValue.value))) {
+        modalOverlayDeposite.classList.remove('hidden');
+        modalOverlayDeposite.classList.add('block');
+        modalDeposite.classList.remove('hidden');
+        modalDeposite.classList.add('block');
+    }
+    else {
+        accountStatus.deposite = accountStatus.deposite + parseInt(depositeValue.value);
+        depositeAmount.innerText = '$' + accountStatus.deposite;
+        accountStatus.balance = accountStatus.balance + parseInt(depositeValue.value);
+        balanceAmount.innerText = '$' + accountStatus.balance;
+
+    }
 
 })
 withdrawButton.addEventListener('click', function () {
     // console.log(withdrawValue.value);
-    accountStatus.withdraw = accountStatus.withdraw + parseInt(withdrawValue.value);
-    withdrawAmount.innerText = '$' + accountStatus.withdraw;
-    accountStatus.balance = accountStatus.balance - parseInt(withdrawValue.value);
-    balanceAmount.innerText = '$' + accountStatus.balance;
+    if (parseInt(withdrawValue.value) > accountStatus.balance) {
+        modalOverlay.classList.remove('hidden');
+        modalOverlay.classList.add('block');
+        modal.classList.remove('hidden');
+        modal.classList.add('block');
+    } else {
+        accountStatus.withdraw = accountStatus.withdraw + parseInt(withdrawValue.value);
+        withdrawAmount.innerText = '$' + accountStatus.withdraw;
+        accountStatus.balance = accountStatus.balance - parseInt(withdrawValue.value);
+        balanceAmount.innerText = '$' + accountStatus.balance;
+
+    }
+})
+closeModal.addEventListener('click', function () {
+    modalOverlay.classList.remove('block');
+    modalOverlay.classList.add('hidden');
+    modal.classList.remove('block');
+    modal.classList.add('hidden');
+})
+closeModalDeposite.addEventListener('click', function () {
+    modalOverlayDeposite.classList.remove('block');
+    modalOverlayDeposite.classList.add('hidden');
+    modalDeposite.classList.remove('block');
+    modalDeposite.classList.add('hidden');
 })
